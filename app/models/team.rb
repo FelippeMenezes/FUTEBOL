317f858c538fef213bc34f9dcf67ece79069b01d
felippemenezes <felippemenezes1982@gmail.com>
2024-11-21 20:33:21 -0300

    create user team wtih cash

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,5 @@
 class Team < ApplicationRecord
+  belongs_to :user, optional: true
   has_many :players, dependent: :destroy
   has_many :home_matches, class_name: 'Match', foreign_key: 'home_team_id', dependent: :destroy
   has_many :away_matches, class_name: 'Match', foreign_key: 'away_team_id', dependent: :destroy
