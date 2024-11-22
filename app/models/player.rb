eeaef6915562da9c3b847efe27c6c83484675fda
felippemenezes <felippemenezes1982@gmail.com>
2024-11-21 22:38:42 -0300

    player name uniqueness

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,7 @@
 class Player < ApplicationRecord
   belongs_to :team
 
+  validates :name, uniqueness: true
   validates :name, :position, presence: true
   validates :accuracy, :strength, :speed, :balance, :agility, :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
