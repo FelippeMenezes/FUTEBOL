e90b730718213e2709be39f0566d8a92ce56800d
felippemenezes <felippemenezes1982@gmail.com>
2024-11-25 20:53:50 -0300

    edit-team-show-styles

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,11 @@ require 'faker'
 # Em cada team, criar 22 jogadores com nome, posição(Goleiro, Zagueiro, Lateral, Meio-Campo, Atacante), accuracy, strength, speed, balance e agility aleatórios entre 51 e 99 e price proporcional a soma dos atributos dividido por 5 vezes 9,87
 
 8.times do |i|
-  team = Team.create!(name: "Team #{i + 1}")
- 
+  team_name = Faker::Sports::Football.team
+  while Team.exists?(name: team_name)
+    team_name = Faker::Sports::Football.team
+  end
+  team = Team.create!(name: team_name)
  
   22.times do
     name = Faker::Name.male_first_name
@@ -11,28 +14,24 @@ require 'faker'
       name = Faker::Name.male_first_name
     end
     position = ["G", "D", "L", "M", "A"].sample
-    accuracy = rand(51..99)
-    strength = rand(51..99)
-    speed = rand(51..99)
-    balance = rand(51..99)
-    accuracy = rand(51..99)
-    agility = rand(51..99)
- 
- 
-    # Supondo que a variável `position` contenha a posição do jogador
+    agility = position == 'G' ? rand(80..99) : rand(31..79)
+    strength = position == 'D' ? rand(80..99) : rand(31..79)
+    speed = position == 'L' ? rand(80..99) : rand(31..79)
+    balance = position == 'M' ? rand(80..99) : rand(31..79)
+    accuracy = position == 'A' ? rand(80..99) : rand(31..79)
+
     case position
     when 'G'
-      price = agility * 51 + strength * 15 + speed * 15 + balance * 15 + accuracy * 15
+      price =  agility* 71 + strength * 10 + speed * 10 + balance * 10 + accuracy * 10
     when 'D'
-      price = strength * 51 + balance * 15 + agility * 15 + accuracy * 15 + speed * 15
+      price = strength * 71 + balance * 10 + agility * 10 + accuracy * 10 + speed * 10
     when 'L'
-      price = speed * 51 + balance * 15 + agility * 15 + accuracy * 15 + strength * 15
+      price = speed * 71 + balance * 10 + agility * 10 + accuracy * 10 + strength * 10
     when 'M'
-      price = balance * 51 + agility * 15 + accuracy * 15 + strength * 15 + speed * 15
+      price = balance * 71 + agility * 10 + accuracy * 10 + strength * 10 + speed * 10
     when 'A'
-      price = accuracy * 51 + speed * 15 + agility * 15 + strength * 15 + balance * 15
+      price = accuracy * 71 + speed * 10 + agility * 10 + strength * 10 + balance * 10
     end
- 
  
     Player.create!(
       name: name,
