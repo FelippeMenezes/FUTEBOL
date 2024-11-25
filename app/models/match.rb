400ce90078b198e49c38b5ca921ba4497c86bbe4
felippemenezes <felippemenezes1982@gmail.com>
2024-11-24 22:43:12 -0300

    create match

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,6 +2,7 @@ class Match < ApplicationRecord
   belongs_to :home_team, class_name: 'Team'
   belongs_to :away_team, class_name: 'Team'
 
-  validates :home_score, :away_score, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :home_team, presence: true
+  validates :away_team, presence: true
 end
 
