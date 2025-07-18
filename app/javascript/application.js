9af4accc748e138833bb8a7285555b99570f2018
Felippe Santana <felippemenezes1982@gmail.com>
2025-07-18 20:27:07 -0300

    alert bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+// Adicione este alerta para ver se o arquivo está sendo lido
+alert("Alerta 1: Application.js foi carregado!");
+
 import "bootstrap"
+
+// Adicione este alerta para ver se a linha APÓS o import é alcançada
+alert("Alerta 2: Bootstrap foi importado!");
